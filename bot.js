3153e0cc0804a3968428767cbff8a3225b465d24
Edouard Courty <edouard.courty2@gmail.com>
2022-06-27 13:42:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,15 +8,6 @@ clientLoader.createClient(['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS'])
   .then(async (client) => {
     await commandLoader.load(client);
 
-    client.on('guildMemberAdd', async (member) => {
-      const guild = member.guild;
-      const role = await guild.roles.fetch('940638422584938606');
-      await member.roles.add(role);
-      // OU
-      await member.roles.add('940638422584938606');
-      // Le role à assigner doit etre placé en dessous du role du bot dans la liste des roles du serveur
-    })
-
     client.on('messageCreate', async (message) => {
       // Ne pas tenir compte des messages envoyés par les bots, ou qui ne commencent pas par le préfix
       if (message.author.bot || !message.content.startsWith(COMMAND_PREFIX)) return;
@@ -24,8 +15,8 @@ clientLoader.createClient(['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS'])
       // On découpe le message pour récupérer tous les mots
       const words = message.content.split(' ');
 
-      const commandName = words[0].slice(1); // Le premier mot du message, auquel on retire le préfix
-      const arguments = words.slice(1); // Tous les mots suivants sauf le premier
+      const commandName = words.shift().slice(1); // Le premier mot du message, auquel on retire le préfix
+      const arguments = words; // Tous les mots suivants sauf le premier
 
       if (client.commands.has(commandName)) {
         // La commande existe, on la lance
